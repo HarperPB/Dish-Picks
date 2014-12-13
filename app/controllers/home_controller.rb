e9802326880fb695c5b3d2624e8cc4f3140a878a
Harper <harper.price.brown@gmail.com>
2014-12-13 14:06:56 -0800

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def search
+    coordinates = { latitude: params[:latitude], longitude: params[:longitude] }
+    render json: Yelp.client.search_by_coordinates(coordinates,{term: params[:term]}, {lang: 'en_CA'})
+    # parameters = { term: params[:term], limit: 16 }
+  end
+end
