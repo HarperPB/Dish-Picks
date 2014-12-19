9453fe18cb010b48304090c333d5343888677abd
Riceleg <legrice@gmail.com>
2014-12-18 21:32:16 -0800

    added feature of 3 pictures & map to restaurants

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,11 +13,11 @@ var nextImage = 0;
 var nearbyPlaces = [];
 var minPriceLevel = 0;
 var firstPlace;
+var hasLoadedFirstImage = false;
 
 // ======================FINDS CURRENT LOCATION =======================
 
 function initiate_geolocation() {                    //function is grabbing lat & long based on users location using html5 function
-console.log('hello')
   // for development
   myLat = 49.282023099999996;
   myLong = -123.1084264;
@@ -63,11 +63,11 @@ function callback(results, status, pagination) {                         //funct
     nearbyPlaces = nearbyPlaces.concat(results);
     console.log(nearbyPlaces)
 
-    populatePhotos(function(firstPlace){               // ADD IF STATEMENT
-      loadImage(firstPlace.photos[0].getUrl({
-        maxHeight: 500,
-        maxWidth: 500
-      }));
+    populatePhotos(function(photo){               // ADD IF STATEMENT
+      hasLoadedFirstImage = true;
+      if ($('.list').children().length == 0){
+        loadImage(photo);
+      }
       if (pagination.hasNextPage){ 
          pagination.nextPage();
         initiate_geolocation();
@@ -99,25 +99,17 @@ var priceRangeSelect = function (e) {
   minPriceLevel = $(this).val();
   $('.list').children().remove();
   photos = [];
-  nearbyPlaces = [];minPriceLevel
-minPriceLevel
-minPriceLevel
+  nearbyPlaces = [];
   initiate_geolocation(); 
 };
 
 
-function loadImage(url) {photos
-  var elem = $('<div><img src="' + url + '"/></div>');
-  $("body").data("current-image-url", url);
+function loadImage(photo) {
+  var elem = $('<div><img src="' + photo.url + '"/></div>');
+  $("body").data("current-image-url", photo.url);
+  $("body").data("current-image-address", photo.address);
   $('.list').html(elem);  // jquery sets elem variable to html image placeholder
 };
-
-function loadImage(url) {photos
-  var elem = $('<div><img src="' + url + '"/></div>');
-  $("body").data("current-image-url", url);
-  $('.list').html(elem);  // jquery sets elem variable to html image placeholder
-};
-
 
 function shuffle(o){ //v1.0
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
@@ -146,21 +138,32 @@ function populatePhotos(success, error) {
       var detailService = new google.maps.places.PlacesService(document.getElementById('hidden-thing'));  // map shit and I think hiding it
 
       detailService.getDetails(detailsRequest, function(placeResult, placeServiceStatus) {    // another API call based on details request which is the placeid
+        
+
+        if(!hasLoadedFirstImage && photos.length > 0){
+          success(photos[0]);     
+        }
         if (placeServiceStatus == google.maps.places.PlacesServiceStatus.OK) {                // if the data is okay
           if(placeResult && placeResult.photos) {   // Do we still need this?                                          // asking for the photo
+            var address = placeResult.name + ' ' + _.inject(placeResult.address_components, function(memo, component) {
+              memo.push(component.long_name);
+              return memo;
+            }, []).join(' ');
             for(i = 0; i < placeResult.photos.length; i++) {
-              tmpPhotos.push(placeResult.photos[i].getUrl({                                                     // sets url to first photo with max height & width  - getUrl is a google api function 
+              tmpPhotos.push({
+                address : address,
+                url : placeResult.photos[i].getUrl({                                                     // sets url to first photo with max height & width  - getUrl is a google api function 
                 maxHeight: 500,
                 maxWidth: 500
-              }));
+              })});
             }
             
           }
         }
-      photos = tmpPhotos
+      photos = shuffle(tmpPhotos);
       });
     }
-    success(nearbyPlaces[0]);
+    
   } else {
     error();
   }
@@ -173,11 +176,23 @@ $(document).ready(function() {
   // bind a click event to clicking on the #goodfood button
   $(".btn-success").on('click', function () {                                              // sets click function on the picture. 
     url = $("body").data("current-image-url");
-    goodfood.push(url);
+    addresses = ($("body").data("current-image-address"));
+    goodfood.push({url : url, addresses : addresses});
+
     if (goodfood.length == 3) {
       $( "button" ).remove();
       $( ".list" ).remove();
+      $.each(goodfood, function( index, value ) {
+      var elem = $('<li><img data-addresses="' + value.addresses + '" src="' + value.url + '"/></li>');
+      $( ".goodfood-container #favorites" ).append(elem);
+      $( ".goodfood-container #favorites" ).css("display","inline");
+      
+      });
 
+
+      $(".goodfood-container #favorites img").on('click', function (e) {
+        window.location = "https://maps.google.ca/maps?q=" + e.target.getAttribute('data-addresses');
+      });
     }
     getNextImage();                                                     // after click gets next nearby place
   });
