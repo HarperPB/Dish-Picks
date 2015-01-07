db83cd7c3c94e9f21d5a92c9c678999eebd73889
Harper <harper.price.brown@gmail.com>
2015-01-06 16:57:19 -0800

    removal of all old code after refactor

--- config/initializers/yelp.rb
+++
@@ -1,6 +0,0 @@
-Yelp.client.configure do |config|
-  config.consumer_key = '7mG3JNYei4cxlf4tC-lIIw'
-  config.consumer_secret = 'PD0m9qnN1FkukZVt6PtksuizJ1s'
-  config.token = '-0btCsR1sF4w9msWp6Z6ZzbAhUz9G2qb'
-  config.token_secret = 'Zg-Wknk4LHo2B5VRWs4tZt31EFM'
-end
